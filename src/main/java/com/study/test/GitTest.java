6ecf9fa66012f063cf2fb86b978bcf457bcecd44
bgl <bgl807856131@163.com>
2017-11-23 23:32:53 +0800

    aaaaaaaaaaa

--- src/main/java/com/study/test/GitTest.java
+++ src/main/java/com/study/test/GitTest.java
@@ -6,6 +6,6 @@ package com.study.test;
 public class GitTest {
     public static void main(String[] args) {
 
-        System.out.println(1111);
+        System.out.println(1122222222211D);
     }
 }
